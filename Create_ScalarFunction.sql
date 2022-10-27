fa92f3b2ab1864a1e2a65b893665fdb4b2671236
Anastasia Shorats <116439591+ana3stay@users.noreply.github.com>
2022-10-27 18:08:25 +0300

    Create Create_ScalarFunction.sql

---
+++ Create_ScalarFunction.sql
@@ -0,0 +1,36 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Scalar Function (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the function.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date, ,>
+-- Description:	<Description, ,>
+-- =============================================
+CREATE FUNCTION ScalarFunction (
+@id INT = 1
+)
+RETURNS int
+AS
+BEGIN
+DECLARE @Res int
+SELECT @Res = COUNT(id)
+FROM _Clients_
+WHERE idClientStatus = @id
+RETURN @Res
+END
+GO
+
+
+
